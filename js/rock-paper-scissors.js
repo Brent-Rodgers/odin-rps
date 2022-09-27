658ad32259ff309c037f536dc6433730fccb1083
Brent-Rodgers <rodgers.brentc@gmail.com>
2022-09-26 19:41:51 -0600

    Add function to randomly generate computer game move

---
+++ js/rock-paper-scissors.js
@@ -0,0 +1,14 @@
+getComputerChoice = function() {
+    let choiceIndex = Math.floor(Math.random()*3)+1;
+    let computerChoice;
+
+    if (choiceIndex === 1) {
+        computerChoice = "rock";
+    } else if (choiceIndex === 2) {
+        computerChoice = "paper";
+    } else if (choiceIndex === 3) {
+        computerChoice = "scissors";
+    }
+
+    return(computerChoice);
+}
